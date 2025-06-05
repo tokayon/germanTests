1b2cc8e372a9a38bd2418ec8a8016e2eeca0c40c
Serge Sinkevych <serge@avicenaheart.com>
2025-06-05 08:32:51 -0700

    Initial Commit

---
+++ GermanTests/GermanTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GermanTestsApp.swift
+//  GermanTests
+//
+//  Created by Serge Sinkevych on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GermanTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
